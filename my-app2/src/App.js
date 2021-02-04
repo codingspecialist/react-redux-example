532ecd16cd42dfc3511ec36481daf8acb23db9f7
ssar <ssar@nate.com>
2021-02-04 13:35:37 +0900

    리덕스 hooks로 수정

---
+++ my-app2/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+import Top from "./components/Top";
+import Bottom from "./components/Bottom";
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>최상단 화면</h1>
+      <Top />
+      <Bottom />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app2/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./App";
+import reducer from "./store";
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
